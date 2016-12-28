7be7507e50f33f44778f1440c323dc7bbb0045cf
Carl Thomé <carlthome@gmail.com>
2016-12-28 15:13:22 +0100

    Split flattening of inputs out into separate function

--- ConvLSTMCell.py
+++ ConvLSTMCell.py
@@ -1,17 +1,18 @@
-from tensorflow.python.ops.variable_scope import variable_scope, get_variable
+from tensorflow.python.ops.array_ops import concat, reshape, split, zeros
 from tensorflow.python.ops.init_ops import constant_initializer, orthogonal_initializer
-from tensorflow.python.ops.math_ops import tanh, sigmoid
-from tensorflow.python.ops.rnn_cell import RNNCell, LSTMStateTuple
-from tensorflow.python.ops.nn_ops import conv2d, conv3d, bias_add
-from tensorflow.python.ops.array_ops import concat, split, reshape, zeros
+from tensorflow.python.ops.math_ops import sigmoid, tanh
+from tensorflow.python.ops.nn_ops import bias_add, conv2d, conv3d
+from tensorflow.python.ops.rnn_cell import LSTMStateTuple, RNNCell
+from tensorflow.python.ops.variable_scope import get_variable, variable_scope
 
 
 class ConvLSTMCell(RNNCell):
   """A LSTM cell with convolutions instead of multiplications.
 
-  Reference:
+  Reference: 
     Xingjian, S. H. I., et al. "Convolutional LSTM network: A machine learning approach for precipitation nowcasting." Advances in Neural Information Processing Systems. 2015.
   """
+
   def __init__(self, filters, height, width, channels, kernel=[3, 3], forget_bias=1.0, activation=tanh, weights_initializer=orthogonal_initializer()):
     self._kernel = kernel
     self._num_units = filters
@@ -30,19 +31,19 @@ class ConvLSTMCell(RNNCell):
   @property
   def output_size(self):
     return self._height * self._width * self._num_units
-    
+
   def zero_state(self, batch_size, dtype):
     shape = [batch_size, self._height * self._width * self._num_units]
     memory = zeros(shape, dtype=dtype)
     output = zeros(shape, dtype=dtype)
     return LSTMStateTuple(memory, output)
 
-  def __call__(self, input, state, scope=None):    
+  def __call__(self, input, state, scope=None):
     with variable_scope(scope or 'ConvLSTMCell'):
       previous_memory, previous_output = state
- 
+
       with variable_scope('Expand'):
-        shape =  [-1, self._height, self._width, self._num_units]
+        shape = [-1, self._height, self._width, self._num_units]
         input = reshape(input, shape)
         previous_memory = reshape(previous_memory, shape)
         previous_output = reshape(previous_output, shape)
@@ -55,37 +56,37 @@ class ConvLSTMCell(RNNCell):
         input_gate, new_input, forget_gate, output_gate = split(3, 4, y)
 
       with variable_scope('LSTM'):
-        memory = (previous_memory
-          * sigmoid(forget_gate + self._forget_bias)
+        memory = (previous_memory 
+          * sigmoid(forget_gate + self._forget_bias) 
           + sigmoid(input_gate) * self._activation(new_input))
         output = self._activation(memory) * sigmoid(output_gate)
-   
+
       with variable_scope('Flatten'):
         shape = [-1, self._height * self._width * self._num_units]
         output = reshape(output, shape)
         memory = reshape(memory, shape)
 
       return output, LSTMStateTuple(memory, output)
-   
+
 
 def convolve_inputs(inputs, filters, kernel=[1, 1, 1], weights_initializer=orthogonal_initializer(), scope=None):
-  s = inputs.get_shape()
-  samples = s[0].value
-  timesteps = s[1].value
-  height = s[2].value
-  width = s[3].value
-  channels = s[4].value
-  
-  with variable_scope('Conv3D'):
-    W = get_variable('Weights', kernel + [channels, filters], initializer=weights_initializer)
-    b = get_variable('Biases', [filters], initializer=constant_initializer(0.0))
+  samples, timesteps, height, width, channels = inputs.get_shape().as_list()
+  with variable_scope(scope or 'Conv3D'):
+    W = get_variable(
+        'Weights',
+        kernel + [channels, filters],
+        initializer=weights_initializer)
+    b = get_variable(
+        'Biases', [filters], initializer=constant_initializer(0.0))
     y = bias_add(conv3d(inputs, W, [1] * 5, 'SAME'), b)
-    return reshape(y, [samples, timesteps, height * width * filters])
-    
+    return y
+
+
+def flatten_inputs(inputs):
+  samples, timesteps, height, width, filters = inputs.get_shape().as_list()
+  return reshape(inputs, [samples, timesteps, height * width * filters])
+
 
 def expand_outputs(outputs, height, width):
-  s = outputs.get_shape()
-  samples = s[0].value
-  timesteps = s[1].value
-  
+  samples, timesteps, features = outputs.get_shape().as_list()
   return reshape(outputs, [samples, timesteps, height, width, -1])
--- README.md
+++ README.md
@@ -2,17 +2,17 @@
 A ConvLSTM cell for TensorFlow's RNN API. 
 
 # About TensorFlow's RNNs
-`tf.nn.dynamic_rnn` requires input to be 3D tensors `(sequence, time, feature)`, while a ConvLSTM takes 5D tensors `(sequence, time, width, height, channel)`. A way of getting around this is to flatten the input and expand the output with reshaping. 
+`tf.nn.dynamic_rnn` requires the input/output shape to be unchanged over time, which becomes problematic for a ConvLSTM because the first iteration will increase the number of channels to the number of filters. In order to get around this, one could do an initial 3D convolution to match the number of filters before calling the RNN API.
 
-`tf.nn.dynamic_rnn` also requires the input/output shape to be unchanged over time, which becomes problematic for a ConvLSTM because the first iteration will increase the number of channels to the number of filters. In order to get around this, one could do an initial 3D convolution to match the number of filters before calling the RNN API.
+`tf.nn.dynamic_rnn` also requires input to be 3D tensors `(sequence, time, feature)`, while a ConvLSTM takes 5D tensors `(sequence, time, width, height, channel)`. A way of getting around this is to flatten the input and expand the output with reshaping. 
 
-Therefore this implementation provides two utility functions, `convolve_inputs` and `expand_outputs`, to deal with this.
+Therefore this implementation provides three utility functions (`convolve_inputs`, `flatten_inputs` and `expand_outputs`) to deal with this.
 
 # Usage
 ```py
 import tensorflow as tf
 
-from ConvLSTMCell import ConvLSTMCell, convolve_inputs, expand_outputs
+from ConvLSTMCell import ConvLSTMCell, convolve_inputs, flatten_inputs, expand_outputs
 
 batch_size = 32
 timesteps = 100
@@ -20,19 +20,23 @@ width = 16
 height = 16
 channels = 3
 filters = 12
+dtype = tf.float32
 
-# Create a placeholder for video sequences.
-inputs = tf.placeholder(tf.float32, [batch_size, timesteps, width, height, channels])
+# Create a placeholder for videos.
+inputs = tf.placeholder(dtype, [batch_size, timesteps, width, height, channels])
 
-# 3D convolve video sequences to match the number of filters in the ConvLSTM.
+# 3D convolve videos to match the number of filters in the ConvLSTM.
 inputs = convolve_inputs(inputs, filters)
+
+# Flatten input because tf.nn.dynamic_rnn only accepts 3D input.
+inputs = flatten_inputs(inputs)
 
 # Add the ConvLSTM step.
 cell = ConvLSTMCell(filters, height, width, channels)
 cell = tf.nn.rnn_cell.MultiRNNCell([cell] * 3)
 cell = tf.nn.rnn_cell.DropoutWrapper(cell, 0.5, 0.5)
-state = cell.zero_state(batch_size, tf.float32)
-outputs, state = tf.nn.dynamic_rnn(cell=cell, inputs=inputs, initial_state=state)
+state = cell.zero_state(batch_size, dtype)
+outputs, state = tf.nn.dynamic_rnn(cell, inputs, initial_state=state)
 
 # Reshape outputs to videos again, because tf.nn.dynamic_rnn only accepts 3D input.
 outputs = expand_outputs(outputs, height, width)
