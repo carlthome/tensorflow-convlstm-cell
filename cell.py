9f5ee692abf65b28621561bebdd1c54e94553a49
Carl Thomé <carlthome@gmail.com>
2017-04-13 18:15:43 +0200

    Add layer normalization

--- cell.py
+++ cell.py
@@ -7,13 +7,14 @@ class ConvLSTMCell(tf.contrib.rnn.RNNCell):
     Xingjian, S. H. I., et al. "Convolutional LSTM network: A machine learning approach for precipitation nowcasting." Advances in Neural Information Processing Systems. 2015.
   """
 
-  def __init__(self, shape, filters, kernel, initializer=None, forget_bias=1.0, activation=tf.tanh):
+  def __init__(self, shape, filters, kernel, initializer=None, forget_bias=1.0, activation=tf.tanh, normalize=True):
     self._kernel = kernel
     self._filters = filters
     self._initializer = initializer
     self._forget_bias = forget_bias
     self._activation = activation
     self._size = tf.TensorShape(shape + [self._filters])
+    self._normalize = normalize
     
   @property
   def state_size(self):
@@ -35,12 +36,23 @@ class ConvLSTMCell(tf.contrib.rnn.RNNCell):
       m = gates
       W = tf.get_variable('kernel', self._kernel + [n, m], initializer=self._initializer)
       y = tf.nn.convolution(x, W, 'SAME')
-      y += tf.get_variable('bias', [m], initializer=tf.constant_initializer(0.0))
+      if not self._normalize:
+        y += tf.get_variable('bias', [m], initializer=tf.constant_initializer(0.0))
       input_contribution, input_gate, forget_gate, output_gate = tf.split(y, 4, axis=3)
+
+      if self._normalize:
+        input_contribution = tf.contrib.layers.layer_norm(input_contribution)
+        input_gate = tf.contrib.layers.layer_norm(input_gate)
+        forget_gate = tf.contrib.layers.layer_norm(forget_gate)
+        output_gate = tf.contrib.layers.layer_norm(output_gate)
 
       memory = (previous_memory
         * tf.sigmoid(forget_gate + self._forget_bias)
         + tf.sigmoid(input_gate) * self._activation(input_contribution))
+
+      if self._normalize:
+        memory = tf.contrib.layers.layer_norm(memory)
+
       output = self._activation(memory) * tf.sigmoid(output_gate)
 
       return output, tf.contrib.rnn.LSTMStateTuple(memory, output)
@@ -49,12 +61,13 @@ class ConvLSTMCell(tf.contrib.rnn.RNNCell):
 class ConvGRUCell(tf.contrib.rnn.RNNCell):
   """A GRU cell with convolutions instead of multiplications."""
 
-  def __init__(self, shape, filters, kernel, initializer=None, activation=tf.tanh):
+  def __init__(self, shape, filters, kernel, initializer=None, activation=tf.tanh, normalize=True):
     self._filters = filters
     self._kernel = kernel
     self._initializer = initializer
     self._activation = activation
     self._size = tf.TensorShape(shape + [self._filters])
+    self._normalize = normalize
 
   @property
   def state_size(self):
@@ -74,8 +87,13 @@ class ConvGRUCell(tf.contrib.rnn.RNNCell):
         m = 2 * self._filters if self._filters > 1 else 2
         W = tf.get_variable('kernel', self._kernel + [n, m], initializer=self._initializer)
         y = tf.nn.convolution(inputs, W, 'SAME')
-        y += tf.get_variable('bias', [m], initializer=tf.constant_initializer(1.0))
-        reset_gate, update_gate = tf.split(y, 2, axis=3)
+        if self._normalize:
+          reset_gate, update_gate = tf.split(y, 2, axis=3)
+          reset_gate = tf.contrib.layers.layer_norm(reset_gate)
+          update_gate = tf.contrib.layers.layer_norm(update_gate)
+        else:
+          y += tf.get_variable('bias', [m], initializer=tf.constant_initializer(1.0))
+          reset_gate, update_gate = tf.split(y, 2, axis=3)
         reset_gate, update_gate = tf.sigmoid(reset_gate), tf.sigmoid(update_gate)
 
       with tf.variable_scope('Output'):
@@ -84,7 +102,10 @@ class ConvGRUCell(tf.contrib.rnn.RNNCell):
         m = self._filters
         W = tf.get_variable('kernel', self._kernel + [n, m], initializer=self._initializer)
         y = tf.nn.convolution(inputs, W, 'SAME')
-        y += tf.get_variable('bias', [m], initializer=tf.constant_initializer(0.0))
+        if self._normalize:
+          y = tf.contrib.layers.layer_norm(y)
+        else:
+          y += tf.get_variable('bias', [m], initializer=tf.constant_initializer(0.0))
         y = self._activation(y)
         output = update_gate * h + (1 - update_gate) * y
 
