5515ebbd359feb31ca269bcd7e4f5b2394780692
Carl Thomé <carlthome@gmail.com>
2017-06-02 12:15:07 +0200

    Add peepholes and update to TensorFlow 1.2

--- cell.py
+++ cell.py
@@ -1,20 +1,21 @@
 import tensorflow as tf
 
-class ConvLSTMCell(tf.contrib.rnn.RNNCell):
+class ConvLSTMCell(tf.nn.rnn_cell.RNNCell):
   """A LSTM cell with convolutions instead of multiplications.
 
   Reference:
     Xingjian, S. H. I., et al. "Convolutional LSTM network: A machine learning approach for precipitation nowcasting." Advances in Neural Information Processing Systems. 2015.
   """
 
-  def __init__(self, shape, filters, kernel, initializer=None, forget_bias=1.0, activation=tf.tanh, normalize=True):
+  def __init__(self, shape, filters, kernel, forget_bias=1.0, activation=tf.tanh, normalize=True, peephole=True, reuse=None):
+    super(ConvLSTMCell, self).__init__(_reuse=reuse)
     self._kernel = kernel
     self._filters = filters
-    self._initializer = initializer
     self._forget_bias = forget_bias
     self._activation = activation
     self._size = tf.TensorShape(shape + [self._filters])
     self._normalize = normalize
+    self._peephole = peephole
     self._feature_axis = self._size.ndims
 
   @property
@@ -25,44 +26,48 @@ class ConvLSTMCell(tf.contrib.rnn.RNNCell):
   def output_size(self):
     return self._size
 
-  def __call__(self, x, h, scope=None):
-    with tf.variable_scope(scope or self.__class__.__name__):
-      previous_memory, previous_output = h
+  def call(self, x, state):
+    c, h = state
 
-      channels = x.shape[-1].value
-      filters = self._filters
-      gates = 4 * filters if filters > 1 else 4
-      x = tf.concat([x, previous_output], axis=self._feature_axis)
-      n = channels + filters
-      m = gates
-      W = tf.get_variable('kernel', self._kernel + [n, m], initializer=self._initializer)
-      y = tf.nn.convolution(x, W, 'SAME')
-      if not self._normalize:
-        y += tf.get_variable('bias', [m], initializer=tf.constant_initializer(0.0))
-      input_contribution, input_gate, forget_gate, output_gate = tf.split(y, 4, axis=self._feature_axis)
+    x = tf.concat([x, h], axis=self._feature_axis)
+    n = x.shape[-1].value
+    m = 4 * self._filters if self._filters > 1 else 4
+    W = tf.get_variable('kernel', self._kernel + [n, m])
+    y = tf.nn.convolution(x, W, 'SAME')
+    if not self._normalize:
+      y += tf.get_variable('bias', [m], initializer=tf.zeros_initializer())
+    j, i, f, o = tf.split(y, 4, axis=self._feature_axis)
 
-      if self._normalize:
-        input_contribution = tf.contrib.layers.layer_norm(input_contribution)
-        input_gate = tf.contrib.layers.layer_norm(input_gate)
-        forget_gate = tf.contrib.layers.layer_norm(forget_gate)
-        output_gate = tf.contrib.layers.layer_norm(output_gate)
+    if self._peephole:
+      i += tf.get_variable('W_ci', c.shape[1:]) * c
+      f += tf.get_variable('W_cf', c.shape[1:]) * c
 
-      memory = (previous_memory
-        * tf.sigmoid(forget_gate + self._forget_bias)
-        + tf.sigmoid(input_gate) * self._activation(input_contribution))
+    if self._normalize:
+      j = tf.contrib.layers.layer_norm(j)
+      i = tf.contrib.layers.layer_norm(i)
+      f = tf.contrib.layers.layer_norm(f)
 
-      if self._normalize:
-        memory = tf.contrib.layers.layer_norm(memory)
+    c = c * tf.sigmoid(f + self._forget_bias) + tf.sigmoid(i) * self._activation(j)
 
-      output = self._activation(memory) * tf.sigmoid(output_gate)
+    if self._peephole:
+      o += tf.get_variable('W_co', c.shape[1:]) * c
 
-      return output, tf.contrib.rnn.LSTMStateTuple(memory, output)
+    if self._normalize:
+      o = tf.contrib.layers.layer_norm(o)
+      c = tf.contrib.layers.layer_norm(c)
+
+    h = tf.sigmoid(o) * self._activation(c)
+
+    state = tf.contrib.rnn.LSTMStateTuple(c, h)
+
+    return h, state
 
 
-class ConvGRUCell(tf.contrib.rnn.RNNCell):
+class ConvGRUCell(tf.nn.rnn_cell.RNNCell):
   """A GRU cell with convolutions instead of multiplications."""
 
-  def __init__(self, shape, filters, kernel, initializer=None, activation=tf.tanh, normalize=True):
+  def __init__(self, shape, filters, kernel, initializer=None, activation=tf.tanh, normalize=True, reuse=None):
+    super(ConvGRUCell, self).__init__(_reuse=reuse)
     self._filters = filters
     self._kernel = kernel
     self._initializer = initializer
@@ -79,36 +84,34 @@ class ConvGRUCell(tf.contrib.rnn.RNNCell):
   def output_size(self):
     return self._size
 
-  def __call__(self, x, h, scope=None):
-    with tf.variable_scope(scope or self.__class__.__name__):
+  def call(self, x, h):
+    channels = x.shape[self._feature_axis].value
 
-      with tf.variable_scope('Gates'):
-        channels = x.shape[-1].value
-        inputs = tf.concat([x, h], axis=self._feature_axis)
-        n = channels + self._filters
-        m = 2 * self._filters if self._filters > 1 else 2
-        W = tf.get_variable('kernel', self._kernel + [n, m], initializer=self._initializer)
-        y = tf.nn.convolution(inputs, W, 'SAME')
-        if self._normalize:
-          reset_gate, update_gate = tf.split(y, 2, axis=self._feature_axis)
-          reset_gate = tf.contrib.layers.layer_norm(reset_gate)
-          update_gate = tf.contrib.layers.layer_norm(update_gate)
-        else:
-          y += tf.get_variable('bias', [m], initializer=tf.constant_initializer(1.0))
-          reset_gate, update_gate = tf.split(y, 2, axis=self._feature_axis)
-        reset_gate, update_gate = tf.sigmoid(reset_gate), tf.sigmoid(update_gate)
+    with tf.variable_scope('gates'):
+      inputs = tf.concat([x, h], axis=self._feature_axis)
+      n = channels + self._filters
+      m = 2 * self._filters if self._filters > 1 else 2
+      W = tf.get_variable('kernel', self._kernel + [n, m])
+      y = tf.nn.convolution(inputs, W, 'SAME')
+      if self._normalize:
+        r, u = tf.split(y, 2, axis=self._feature_axis)
+        r = tf.contrib.layers.layer_norm(r)
+        u = tf.contrib.layers.layer_norm(u)
+      else:
+        y += tf.get_variable('bias', [m], initializer=tf.ones_initializer())
+        r, u = tf.split(y, 2, axis=self._feature_axis)
+      r, u = tf.sigmoid(r), tf.sigmoid(u)
 
-      with tf.variable_scope('Output'):
-        inputs = tf.concat([x, reset_gate * h], axis=self._feature_axis)
-        n = channels + self._filters
-        m = self._filters
-        W = tf.get_variable('kernel', self._kernel + [n, m], initializer=self._initializer)
-        y = tf.nn.convolution(inputs, W, 'SAME')
-        if self._normalize:
-          y = tf.contrib.layers.layer_norm(y)
-        else:
-          y += tf.get_variable('bias', [m], initializer=tf.constant_initializer(0.0))
-        y = self._activation(y)
-        output = update_gate * h + (1 - update_gate) * y
+    with tf.variable_scope('candidate'):
+      inputs = tf.concat([x, r * h], axis=self._feature_axis)
+      n = channels + self._filters
+      m = self._filters
+      W = tf.get_variable('kernel', self._kernel + [n, m])
+      y = tf.nn.convolution(inputs, W, 'SAME')
+      if self._normalize:
+        y = tf.contrib.layers.layer_norm(y)
+      else:
+        y += tf.get_variable('bias', [m], initializer=tf.zeros_initializer())
+      h = u * h + (1 - u) * self._activation(y)
 
-      return output, output
+    return h, h
